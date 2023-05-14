b5bb9ea12505285af1f6b7b6189ecb22161ffe67
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-05-14 12:21:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,31 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const { off } = require('../models/user.js');
-const users   =require("../models/user.js");
-
+const mongoose = require("mongoose");
+const { off } = require("../models/user.js");
+const users = require("../models/user.js");
 
 // Import routes
 
 //Router Middlewares
 app.use(express.json());
 
+app.get("/", async function (req, res) {
+  var limit = req.query.limit,
+    offset = req.query.offset;
 
-//default value for limit is 5 and offset is 0
-//This route should return an array of _id of all the element that need to be returned.
-//output id can be in any order.
+  if (limit == null) limit = 5;
+  else limit = parseInt(limit);
+  if (offset == null) offset = 0;
+  else offset = parseInt(offset);
 
-app.get("/",async function(req,res){
+  if (limit > 5) limit = 5;
 
-    var ids = [];
+  result = await users.find({});
 
-    //Write your code here
-    //modify the ids array
+  var ids = [];
+  const startt = limit * offset;
 
-    res.send(ids);
+  for (var i = startt; i < result.length && i < startt + limit; i++)
+    ids.push(result[i]["_id"]);
 
+  res.send(ids);
 });
 
 module.exports = app;
-
-
